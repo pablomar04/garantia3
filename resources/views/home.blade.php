b82e494ed0a3d6c15030a876547342b3f286303e
Juan Pablo Marcovecchio <pablo_mar04@hotmail.com>
2018-01-26 17:19:37 -0300

    home.blade.php terminado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,83 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading"><h2>Bienvenido a Garantías Admin</h2></div>
-
+                <div class="panel-heading"><h2><strong>Garantía</strong></h2></div>
                 <div class="panel-body">
+                	<h3><strong>Paquete de garantía para todos los modelos</strong></h3>
+                	<ul class="list-group">
+					  <li class="list-group-item">3 años de cobertura contra defectos o 100.000 Km (lo que se cumpla primero)</li>
+					  <li class="list-group-item">3 años de cobertura contra defectos de Pintura (kilometraje ilimitado)</li>
+					  <li class="list-group-item">6 años de cobertura contra defectos de Corrosión (kilometraje ilimitado)</li>
+					</ul> 
+					<h3><strong>Garantía limitada</strong></h3>
+					<h4><u>6 meses de cobertura contra defectos o 10.000 Km (lo que se cumpla primero)</u></h4>
+					<ul class="list-group">
+						<li class="list-group-item">Lámparas (no incluye Xenón y Led) y fusibles.</li>
+						<li class="list-group-item">Pastillas, discos y zapatas de freno.</li>
+						<li class="list-group-item">Filtros.</li>
+						<li class="list-group-item">Bujías.</li>
+						<li class="list-group-item">Escobillas limpiaparabrisas y luneta.</li>
+						<li class="list-group-item">Correas auxiliares.</li>
+					</ul>
+					<h4><u>1 año de cobertura contra defectos o 20.000 Km (lo que se cumpla primero)</u></h4>
+					<ul class="list-group">
+						<li class="list-group-item">Amortiguadores de suspensión (no incluye amortiguadores neumáticos).</li>
+					</ul>
+					<h4><u>1 año de cobertura contra defectos o 30.000 Km (lo que se cumpla primero)</u></h4>
+					<ul class="list-group">
+						<li class="list-group-item">Collarín, placa y disco de embrague (cambio manual)</li>
+					</ul>
+					<h4><u>Baterías (ver procedimiento)</u></h4>
+					<ul class="list-group">
+						<li class="list-group-item">Hasta 12 meses o 100.000 km - 100% de reconocimiento.</li>
+						<li class="list-group-item">Hasta 24 meses o 100.000 km - 50% de reconocimiento.</li>
+					</ul>
+					<h4><u>Solo en Pre entrega</u></h4>
+					<ul class="list-group">
+						<li class="list-group-item">Geometría de ejes.</li>
+						<li class="list-group-item">Balanceo de ruedas.</li>
+					</ul>
+					<h3><strong>Garantía de repuestos</strong></h3>
+					<h4><u>Piezas instaladas sin cargo al Propietario</u></h4>
+					<p>Se refiere a aquellas reparaciones o reemplazos de piezas que han sido reparadas o reemplazadas sin cargo, en el taller del Concesionario, en vehículos que se encuentran dentro del período de Garantía.
+<strong>La Garantía de la pieza instalada toma el período de Garantía de la unidad y no superará la de esta.</strong></p> 					   					   					    					            	
+					<ul class="list-group">
+						<li class="list-group-item">Cubre la Mano de Obra necesaria para reemplazar la pieza defectuosa y las piezas dañadas como consecuencia.</li>
+						<li class="list-group-item">Cubre la pieza defectuosa y las piezas dañadas por consecuencia.</li>
+					</ul>
+					<h4><u>Piezas instaladas con cargo al Propietario</u></h4>
+					<p>Se refiere a piezas vendidas por el Concesionario para ser colocados en su taller, en unidades que están dentro o fuera del período de Garantía, manteniendo la Garantía de la pieza. Las piezas involucradas en este criterio son aquellos instaladas con cargo al Propietario y toman el período de <strong>Garantía de 1 año.</strong></p> 					   					   					    					            	
+					<ul class="list-group">
+						<li class="list-group-item">Piezas consideradas componentes de desgaste, poseen el período de Garantía según tabla “Componentes con Garantía Limitada”</li>
+						<li class="list-group-item">Cubre la Mano de Obra necesaria para reemplazar la pieza defectuosa y las piezas dañadas como consecuencia.</li>
+						<li class="list-group-item">Cubre la pieza defectuosa y las piezas dañadas por consecuencia.</li>
+					</ul>
+					<h4><u>Repuestos vendidos por mostrador</u></h4>
+					<ul class="list-group">
+						<li class="list-group-item">Cubre el valor de la pieza reemplazada por el <strong>período de 1 año.</strong></li>
+						<li class="list-group-item">Piezas consideradas componentes de desgaste, poseen el período de Garantía según tabla “Componentes con Garantía Limitada”</li>
+						<li class="list-group-item">No cubre la Mano de Obra necesaria para desmontar la pieza defectuosa, ni la de instalar la nueva, ni tampoco consecuentes de la reparación.</li>
+						<li class="list-group-item">No cubre gastos de remolque.</li>
+						<li class="list-group-item">No cubre desgaste normal.</li>
+						<li class="list-group-item">No cubre piezas eléctricas/electrónicas donde se compruebe que la avería ha sido provocada por una incorrecta conexión.</li>
+						<li class="list-group-item">En los casos particulares donde se requiera la utilización de Herramientas Especiales y acceso a Sistemas del Grupo para la instalación y posterior ajuste del componente, la responsabilidad de demostrar que la avería se debe a una falla de producto, y que la instalación se realizó siguien-do las indicaciones del Manual de Reparaciones, es exclusiva del Concesionario Vendedor.</li>					
+					</ul>
+					<h3><strong>Garantía de accesorios</strong></h3>												
+					<p>Todos los Accesorios Originales Volkswagen y piezas extras opcionales comercializados por Volkswagen Argentina S.A. a través de su red de Concesionarios (por ejemplo: accesorios de radio y teléfono, barras de remolque) serán reemplazados sin cargo si mostraran evidencias de defectos o fallas de materiales, manufactura o funcionamiento, dentro de su término de Garantía a partir de su fecha de instalación en la red de Concesionarios o fecha de factura de venta del Concesionario.</p>
+					<h4><u>Accesorios Originales Volkswagen instaladas con cargo al Propietario</u></h4>
+					<p>Se refiere a Accesorios Originales Volkswagen vendidos por el Concesionario para ser instalados en su taller, en unidades que están dentro o fuera del período de Garantía.</p>
+					<p>Los accesorios Originales involucrados en este criterio son aquellos instaladas con cargo al Propietario y toman el <strong>período de Garantía de 1 año.</strong></p>
+					<ul class="list-group">
+						<li class="list-group-item">Cubre la Mano de Obra necesaria para reemplazar el accesorio defectuoso y los componentes dañados como consecuencia.</li>
+						<li class="list-group-item">Cubre el accesorio defectuoso, y el de los componentes dañados por consecuencia.</li>
+					</ul>
+					<h4><u>Accesorios Originales Volkswagen vendidos por mostrador</u></h4>
+					<ul class="list-group">
+						<li class="list-group-item">Cubre el valor del accesorio reemplazado por el <strong>período de 1 año.</strong></li>
+						<li class="list-group-item">No cubre la Mano de Obra necesaria para desmontar el accesorio defectuoso ni la de instalar el nuevo, ni tampoco consecuentes de la reparación.</li>
+						<li class="list-group-item">No cubre desgaste normal.</li>
+						<li class="list-group-item">No cubre piezas eléctricas/electrónicas donde se compruebe que la avería ha sido provocada por una incorrecta conexión.</li>						
+					</ul>										
                 </div>
             </div>
         </div>
